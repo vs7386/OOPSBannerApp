2901cc8cf3eed8b9ddd8d6c000330cff6e5818b8
moonbreakers <moonbreakers@moonbreakerss-MacBook-Pro.local>
2026-03-03 10:47:16 +0530

    UC2(Use Case 2)

---
+++ UC2.java
@@ -0,0 +1,24 @@
+/**
+ * OOPSBannerApp UC2 – OOPS Banner Display Application
+ *
+ * This class demonstrates a simple Java application that displays
+ * the Object-Oriented Programming System (OOPS) acronym
+ * in a banner format using asterisks (*) and spaces.
+ *
+ * @author Srirangam Venkata Shrawan
+ * @version 21.0
+ */
+
+public class UC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" ******** " + "  ******** " + "  ******** " + "  ******** ");
+        System.out.println(" *      * " + "  *      * " + "  *      * " + "  *        ");
+        System.out.println(" *      * " + "  *      * " + "  *      * " + "  *        ");
+        System.out.println(" *      * " + "  *      * " + "  ******** " + "  ******** ");
+        System.out.println(" *      * " + "  *      * " + "  *        " + "         * ");
+        System.out.println(" *      * " + "  *      * " + "  *        " + "         * ");
+        System.out.println(" ******** " + "  ******** " + "  *        " + "  ******** ");
+    }
+}
